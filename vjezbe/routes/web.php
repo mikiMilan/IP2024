a90d6b43db48856b2c01420465521e166eff4418
mickoMilan <predojevic.milan@yahoo.com>
2024-02-26 13:39:46 +0100

    rute

--- vjezbe/routes/web.php
+++ vjezbe/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/kuca', function () {
+    $auto = ['BMV', 'AUDI'];
+    return view('auta', ["auta"=>$auto]);
+});
+
